c5f44e62ff6abb261f663e844fda0185c9cec8ed
RyanIrfandi <ryfan1993@engineer.com>
2015-09-12 17:11:35 +0700

    praktilum 01 kuliah02

---
+++ Prak-05-Kebun-Binatang/Kambing.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Kambing here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ * String suara="Mbek";
+    int jumlahKaki=4;
+ */
+public class Kambing implements Mamalia{
+    
+        public String suara(){
+      String suara="Mbek";
+      return suara;
+    }
+  
+  public int jumlahKaki (){
+      int jumlahKaki=4;
+      return jumlahKaki;
+    }
+}
